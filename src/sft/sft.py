d6fc9c16b211033911d4fe1b34fb3069181b3bbf
LinuzJ <linusjern00@gmail.com>
2025-02-10 14:11:48 +0200

    sft training and slurm

--- src/sft/sft.py
+++ src/sft/sft.py
@@ -1,8 +1,6 @@
 import os
-import sys
 from dataclasses import dataclass, field, asdict
 from typing import Optional
-import os
 import trl
 
 import warnings
@@ -23,13 +21,63 @@ class TrainingConfig:
     model_name: str = field(default="Qwen/Qwen2.5-32B-Instruct")
     block_size: int = field(default=32768)
     wandb_project: Optional[str] = field(default="quantum-circuit-generation")
-    wandb_entity: Optional[str] = field(default="hashimoto-group")
-    train_file_path: Optional[str] = field(default="simplescaling/s1K_tokenized")
+    wandb_entity: Optional[str] = field(default="linusjern")
+    train_file_path: Optional[str] = field(
+        default="linuzj/hypergraph-max-cut-quantum_tokenized"
+    )
     dagger: bool = field(default=False)
 
     def __post_init__(self):
         os.environ["WANDB_PROJECT"] = self.wandb_project
         os.environ["WANDB_ENTITY"] = self.wandb_entity
+
+
+def train():
+    parser = transformers.HfArgumentParser((TrainingConfig, trl.SFTConfig))
+    config, args = parser.parse_args_into_dataclasses()
+
+    log_config = {**asdict(config), **asdict(args)}
+    logging.info(f"Training config: {log_config}")
+
+    # ----- Load Model, Data and Tokenizer -----
+    model = transformers.AutoModelForCausalLM.from_pretrained(config.model_name)
+    dataset = load_dataset(config.train_file_path)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        config.model_name, use_fast=True
+    )
+
+    # ----- Setup Instruction Templates -----
+    instruction_template = "<|im_start|>user"
+    response_template = "<|im_start|>assistant"
+    tokenizer.pad_token = "<|fim_pad_token|>"
+
+    # Only compute loss over assistant responses
+    # Verified that it precisely starts where the thinking tokens start and ends with the first pad token
+    # via labels being set to -100
+    collator = trl.DataCollatorForCompletionOnlyLM(
+        instruction_template=instruction_template,
+        response_template=response_template,
+        tokenizer=tokenizer,
+        mlm=False,
+    )
+
+    args.dataset_text_field = "text"
+    args.max_seq_length = config.block_size
+
+    # ----- Setup Trainer -----
+    trainer = trl.SFTTrainer(
+        model,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"] if "test" in dataset else dataset["train"],
+        args=args,
+        data_collator=collator,
+    )
+
+    # ----- Train Model -----
+    trainer.train()
+    trainer.save_model(output_dir=args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
+    trainer.accelerator.wait_for_everyone()
 
 
 if __name__ == "__main__":
