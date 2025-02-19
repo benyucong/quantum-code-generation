51b26627b5988b9e81790bc79547dde5e771bcdf
LinuzJ <linusjern00@gmail.com>
2025-02-19 17:46:17 +0200

    new validation

--- code/evaluation/evaluate_samples.sh
+++ code/evaluation/evaluate_samples.sh
@@ -1,4 +1,4 @@
-path="../generation/out/quantum_circuits_output_20250219_144925.json"
+path="../generation/out/quantum_circuits_output_20250219_173352.json"
 out="./out/quantum_ciruits_validated.json"
 
 python3 src/evaluate_samples.py $path $out
