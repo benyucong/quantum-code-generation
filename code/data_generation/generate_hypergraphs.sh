01e3cffaf2313082483dd62b8a51be441722e353
LinuzJ <linusjern00@gmail.com>
2025-02-25 16:37:23 +0200

    support

--- code/data_generation/generate_hypergraphs.sh
+++ code/data_generation/generate_hypergraphs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-problem="kclique"
+# Supported problems are: "hypermaxcut", "community_detection", "graph_coloring", "connected_components"
+# Supported ansatz templates are: 1 - 19
+
+problem="graph_coloring"
 ansatz=13
 layers=1
 output_dir="out/"
