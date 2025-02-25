d461fce94e6bbaf3ebcfcd2bec11ad5b4abf2325
LinuzJ <linusjern00@gmail.com>
2025-02-25 16:44:06 +0200

    update slurm script to generate data

--- code/data_generation/training_data_gen.sh
+++ code/data_generation/training_data_gen.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=trainingdata_gen_batch
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=../../logs/hypermaxcut_%A_%a.out
 #SBATCH --error=../../logs/hypermaxcut_%A_%a.err
-#SBATCH --array=0-2
+#SBATCH --array=0-15
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=50GB
-##SBATCH --gres=gpu:3
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-user=linus.jern@aalto.fi
 
@@ -22,16 +21,26 @@ pip install -r requirements.txt
 pip install custatevec_cu12
 pip install pennylane-lightning-gpu
 
-ANSATZ_OPTIONS=("5" "6" "13")
-OPTION=${ANSATZ_OPTIONS[$SLURM_ARRAY_TASK_ID]}
+PROBLEMS=("hypermaxcut" "community_detection" "graph_coloring" "connected_components")
+ANSATZ_OPTIONS=(1 5 12 13)
 
-echo "Running hypermaxcut with ansatz option ${OPTION}..."
+# Get counts
+NUM_ANSATZ=${#ANSATZ_OPTIONS[@]}   # 4
+NUM_PROBLEMS=${#PROBLEMS[@]}       # 4
+
+PROBLEM_INDEX=$(( SLURM_ARRAY_TASK_ID / NUM_ANSATZ ))
+ANSATZ_INDEX=$(( SLURM_ARRAY_TASK_ID % NUM_ANSATZ ))
+
+SELECTED_PROBLEM=${PROBLEMS[$PROBLEM_INDEX]}
+SELECTED_ANSATZ=${ANSATZ_OPTIONS[$ANSATZ_INDEX]}
+
+echo "Running ${SELECTED_PROBLEM} with ansatz option ${SELECTED_ANSATZ}..."
 
 layers=1
 output_dir="out/"
 
 python3 -m src.main \
-    --hypermaxcut \
+    --problem ${SELECTED_PROBLEM} \
     --layers ${layers} \
-    --ansatz_template ${OPTION} \
+    --ansatz_template ${SELECTED_ANSATZ} \
     --output_path="${output_dir}"
